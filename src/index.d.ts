bebcbf9d884a0f1db02102f96df87afbdb730b62
Lewis Headden <lewis@lewisheadden.com>
2018-11-21 22:27:47 -0500

    Generify OptionValue

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,60 +1,60 @@
 import * as React from 'react';
 
-export interface OptionValue {
+export interface OptionValue<T> {
   label: string;
-  value: any;
+  value: T;
 }
 
-export interface SimpleSelectEvent {
+export interface SimpleSelectEvent<T> {
   originalEvent: Event;
-  value: OptionValue;
+  value: OptionValue<T>;
   open: boolean;
 }
 
-export interface MultipleSelectEvent {
+export interface MultipleSelectEvent<T> {
   originalEvent: Event;
-  values: OptionValue[];
+  values: OptionValue<T>[];
   open: boolean;
 }
 
-export interface SimpleSelectProps {
+export interface SimpleSelectProps<T> {
   autofocus?: boolean;
   cancelKeyboardEventOnSelection?: boolean;
   className?: string;
-  createFromSearch?(items: OptionValue[], search: string): OptionValue;
-  defaultValue?: OptionValue;
+  createFromSearch?(items: OptionValue<T>[], search: string): OptionValue<T>;
+  defaultValue?: OptionValue<T>;
   delimiters?: [any];
   disabled?: boolean;
   dropdownDirection?: number;
-  editable?(item: OptionValue): string;
-  filterOptions?(items: OptionValue[], search: string): OptionValue[];
-  firstOptionIndexToHighlight?(index: number, items: OptionValue[], item: OptionValue, search: string): number;
-  groupId?(item: OptionValue): any;
+  editable?(item: OptionValue<T>): string;
+  filterOptions?(items: OptionValue<T>[], search: string): OptionValue<T>[];
+  firstOptionIndexToHighlight?(index: number, items: OptionValue<T>[], item: OptionValue<T>, search: string): number;
+  groupId?(item: OptionValue<T>): any;
   groups?: any[];
   groupsAsColumns?: boolean;
   hideResetButton?: boolean;
   highlightedUid?: any;
   name?: string;
   open?: boolean;
-  onBlur?(event: SimpleSelectEvent): void;
-  onFocus?(event: SimpleSelectEvent): void;
+  onBlur?(event: SimpleSelectEvent<T>): void;
+  onFocus?(event: SimpleSelectEvent<T>): void;
   onHighlightedUidChange?(uid: any): void;
   onKeyboardSelectionFailed?(keycode: number): void;
   onOpenChange?(open: boolean): void;
-  onPaste?(event: SimpleSelectEvent): void;
+  onPaste?(event: SimpleSelectEvent<T>): void;
   onSearchChange?(search: string): void;
-  onValueChange?(item: OptionValue): void;    
-  options?: OptionValue[];
+  onValueChange?(item: OptionValue<T>): void;
+  options?: OptionValue<T>[];
   placeholder?: string;
   renderGroupTitle?(index: number, group: any): React.ReactElement<any>;
-  renderNoResultsFound?(item: OptionValue, search: string): React.ReactElement<any>;
-  renderOption?(item: OptionValue): React.ReactElement<any>;
+  renderNoResultsFound?(item: OptionValue<T>, search: string): React.ReactElement<any>;
+  renderOption?(item: OptionValue<T>): React.ReactElement<any>;
   renderResetButton?(): React.ReactElement<any>;
   renderToggleButton?(options: {open: boolean, flipped: any}): React.ReactElement<any>;
-  renderValue?(item: OptionValue): React.ReactElement<any>;
-  restoreOnBackspace?(item: OptionValue): string;
+  renderValue?(item: OptionValue<T>): React.ReactElement<any>;
+  restoreOnBackspace?(item: OptionValue<T>): string;
   search?: string;
-  serialize?(item: OptionValue): string;
+  serialize?(item: OptionValue<T>): string;
   style?: any;
   tether?: boolean;
   'tether-props'?: any;
@@ -63,33 +63,33 @@ export interface SimpleSelectProps {
   transitionEnterTimeout?: number;
   transitionLeave?: boolean;
   transitionLeaveTimeout?: number;
-  uid?(item: OptionValue): any;
-  value?: OptionValue;
-  valueFromPaste?(options: OptionValue[], value: OptionValue, pastedText: string): OptionValue;
+  uid?(item: OptionValue<T>): any;
+  value?: OptionValue<T>;
+  valueFromPaste?(options: OptionValue<T>[], value: OptionValue<T>, pastedText: string): OptionValue<T>;
 }
 
-declare class SimpleSelect extends React.Component<SimpleSelectProps, any> {
+declare class SimpleSelect<T> extends React.Component<SimpleSelectProps<T>, any> {
   
 }
 
-export interface MultiSelectProps extends SimpleSelectProps {
-  anchor?: OptionValue;
-  createFromSearch?(items: OptionValue[], search: string): OptionValue;
-  createFromSearch?(options: OptionValue[], values: OptionValue[], search: string): OptionValue;
-  defaultValues?: OptionValue[];
-  filterOptions?(items: OptionValue[], search: string): OptionValue[];
-  filterOptions?(options: OptionValue[], values: OptionValue[], search: string): OptionValue[];
-  onAnchorChange?(item: OptionValue): void;
-  onBlur?(event: SimpleSelectEvent): void;
-  onBlur?(event: MultipleSelectEvent): void;
-  onFocus?(event: SimpleSelectEvent): void;
-  onFocus?(event: MultipleSelectEvent): void;
-  onValuesChange?(item: OptionValue): void;
+export interface MultiSelectProps<T> extends SimpleSelectProps<T> {
+  anchor?: OptionValue<T>;
+  createFromSearch?(items: OptionValue<T>[], search: string): OptionValue<T>;
+  createFromSearch?(options: OptionValue<T>[], values: OptionValue<T>[], search: string): OptionValue<T>;
+  defaultValues?: OptionValue<T>[];
+  filterOptions?(items: OptionValue<T>[], search: string): OptionValue<T>[];
+  filterOptions?(options: OptionValue<T>[], values: OptionValue<T>[], search: string): OptionValue<T>[];
+  onAnchorChange?(item: OptionValue<T>): void;
+  onBlur?(event: SimpleSelectEvent<T>): void;
+  onBlur?(event: MultipleSelectEvent<T>): void;
+  onFocus?(event: SimpleSelectEvent<T>): void;
+  onFocus?(event: MultipleSelectEvent<T>): void;
+  onValuesChange?(item: OptionValue<T>): void;
   maxValues?: number;
   closeOnSelect?: boolean;
-  valuesFromPaste?(options: OptionValue[], values: OptionValue[], pastedText: string): OptionValue[];
+  valuesFromPaste?(options: OptionValue<T>[], values: OptionValue<T>[], pastedText: string): OptionValue<T>[];
 }
 
-declare class MultiSelect extends React.Component<MultiSelectProps, any> {
+declare class MultiSelect<T> extends React.Component<MultiSelectProps<T>, any> {
   
 }
